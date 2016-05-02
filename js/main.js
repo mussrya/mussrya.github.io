2619f2a564574a88b98fd6def4cac5d3549090cf
mussrya <ryan.musselwhite@gmail.com>
2016-05-02 20:33:25 +0100

    Making images larger when clicking on an item

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ portfolioApp.menuLinks = function ()
     setTimeout(function ()
     {
         $('.u-portfolioEntry').html(
-            '<div class="u-innerBlock"><div class="row-fluid"><span class="u-closeButton u-animate">X</span><h1 class="u-animate">' + title + '</h1> <h3 class="u-animate">' + skills + '</h3> <p class="u-marginTop u-animate">' + content + '</p></div><div class="row-fluid u-marginTop"><div class="col-md-4 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/img/'+port+'/1.png"></div></div><div class="col-md-4 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/img/'+port+'/2.png"></div></div><div class="col-md-4 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/img/'+port+'/3.png"></div></div></div></div>');
+            '<div class="u-innerBlock"><div class="row-fluid"><span class="u-closeButton u-animate">X</span><h1 class="u-animate">' + title + '</h1> <h3 class="u-animate">' + skills + '</h3> <p class="u-marginTop u-animate">' + content + '</p></div><div class="row-fluid u-marginTop"><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/img/'+port+'/1.png"></div></div><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/img/'+port+'/2.png"></div></div><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/img/'+port+'/3.png"></div></div></div></div>');
         $('.u-closeButton').unbind('click').bind('click', function ()
         {
             portfolioApp.removeContent();
