7076ed7f64e827a3c1b7a63cd2e8de57607c46c0
mussrya <ryan.musselwhite@gmail.com>
2016-05-08 19:47:51 +0100

    Building new preview page

--- js/main.js
+++ js/main.js
@@ -21,26 +21,26 @@ portfolioApp.contentClick = function ()
             case 1:
                 portfolioApp.outputContent('Objective Manager', 'JavaScript OO, CSS3, HTML5, Animation, BootStrap',
                     'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
-                    'port-1');
+                    'port-1', 'https://about.objectivemanager.com');
                 break;
             case 2:
                 portfolioApp.outputContent('CycleAnalytics', 'AngularJS, NodeJS, CSS3, HTML5, Animation, BootStrap',
                     'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
-                    'port-2');
+                    'port-2', 'http://cycleanalytics.io');
                 break;
             case 3:
                 portfolioApp.outputContent('Reus Boostrap Theme', 'jQuery, CSS3, HTML5, Animation, BootStrap',
                     'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
-                    'port-3');
+                    'port-3', 'http://mussrya.github.io/reus');
                 break;
             case 4:
                 portfolioApp.outputContent('GitRead', 'AngularJS, GitHub API, CSS3, HTML5, BootStrap',
                     'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
-                    'port-4');
+                    'port-4', 'https://github.com/mussrya/GitHub-Release-Notes-Generator');
                 break;
         }
     });
-}, portfolioApp.outputContent = function (title, skills, content, port)
+}, portfolioApp.outputContent = function (title, skills, content, port, link)
 {
     $('body').prepend('<div class="u-portfolioEntry inactive text-center"></div>');
     setTimeout(function ()
@@ -49,7 +49,7 @@ portfolioApp.contentClick = function ()
     }, 100);
     setTimeout(function ()
     {
-        $('.u-portfolioEntry').html('<div class="row-fluid"><div class="col-md-3" style="padding: 0;height: 100%;"><div class="menuBlock col-md-3"><h1 class="u-animate">'+title+'</h1><h3 class="u-animate">'+skills+'</h3><p class="u-marginTop u-animate">'+content+'</p><a href="#" class="buttonCustom"></a></div></div><div class="col-md-9"><span class="u-closeButton u-animate">X</span><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/portfolio/img/'+port+'/JPEG/1.jpg"></div></div><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/portfolio/img/'+port+'/JPEG/1.jpg"></div></div><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/portfolio/img/'+port+'/JPEG/1.jpg"></div></div></div></div>');
+        $('.u-portfolioEntry').html('<div class="row-fluid"><div class="col-md-3" style="padding: 0;height: 100%;"><div class="menuBlock col-md-3"><h1 class="u-animate">'+title+'</h1><h3 class="u-animate">'+skills+'</h3><p class="u-marginTop u-animate">'+content+'</p><a href="'+link+'" class="buttonCustom">VIEW</a></div></div><div class="col-md-9"><span class="u-closeButton u-animate">X</span><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/portfolio/img/'+port+'/JPEG/1.jpg"></div></div><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/portfolio/img/'+port+'/JPEG/1.jpg"></div></div><div class="col-lg-12 u-animate"><div class="u-thumbnail"><img class="img-responsive" src="/portfolio/img/'+port+'/JPEG/1.jpg"></div></div></div></div>');
         $('.u-closeButton').unbind('click').bind('click', function ()
         {
             portfolioApp.removeContent();
