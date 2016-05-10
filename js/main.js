aadf6457afbe4c4d8959b98facd9c9e9c0fb215a
mussrya <ryan.musselwhite@gmail.com>
2016-05-10 22:11:35 +0100

    Added another project

--- js/main.js
+++ js/main.js
@@ -19,28 +19,28 @@ portfolioApp.contentClick = function ()
     {
         switch ($(this).data('entry')) {
             case 1:
-                portfolioApp.outputContent('Objective Manager', 'JavaScript OO, CSS3, HTML5, Animation, BootStrap',
-                    'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
+                portfolioApp.outputContent('ObjectiveManager', 'JavaScript OO, jQuery, CSS3, HTML5, CSS3 Animation, Responsive, BootStrap, GIT',
+                    'After working for ObjectiveManager on their core product, I was asked to build their marketing site using up to date technologies, with the aim to keep it as lightweight as possible so that non front-end developers could still easily maintain the code if required.',
                     'port-1', 'https://about.objectivemanager.com');
                 break;
             case 2:
-                portfolioApp.outputContent('CycleAnalytics', 'AngularJS, NodeJS, CSS3, HTML5, Animation, BootStrap',
-                    'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
-                    'port-2', 'http://cycleanalytics.io');
+                portfolioApp.outputContent('CycleAnalytics', 'AngularJS, NodeJS (API), CSS3, HTML5, CSS3 Animation, Responsive, BootStrap, MongoDB, GIT',
+                    'I built the CycleAnalytics site as a side project to broaden my experience with building a web application end to end, featuring a NodeJS API, NodeJS data grabber and an AngularJS front-end. I managed to represent government data on the cycle hire stations within London in an easy to view format.',
+                    'port-2', 'http://cycleanalytics.io', 'https://github.com/mussrya/Cycle-analytics');
                 break;
             case 3:
-                portfolioApp.outputContent('Reus Boostrap Theme', 'jQuery, CSS3, HTML5, Animation, BootStrap',
-                    'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
-                    'port-3', 'http://mussrya.github.io/reus');
+                portfolioApp.outputContent('Reus Boostrap Theme', 'jQuery, CSS3, HTML5, CSS3 Animation, Responsive, BootStrap',
+                    'As a side project and mostly for fun, I decided to build a theme for Bootstrap which I decided to post to Git for users to download for free. This was mostly a side project to try and use bootstrap to it\'s full effect.',
+                    'port-3', 'http://ryan-musselwhite.com/reus/', 'https://github.com/mussrya/reus');
                 break;
             case 4:
                 portfolioApp.outputContent('GitRead', 'AngularJS, GitHub API, CSS3, HTML5, BootStrap',
-                    'This project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla blaThis project was an example of how I managed to bla bla bla bla',
+                    'Whilst working for a company who used Git Issues to track bugs, I decided to build an application which could easily produce a release note to be sent out to the team based on a particular milestone. I mostly learnt about GitHub APIs in this project as I had extensive experience with AngularJS prior to this project.',
                     'port-4', 'https://github.com/mussrya/GitHub-Release-Notes-Generator');
                 break;
         }
     });
-}, portfolioApp.outputContent = function (title, skills, content, port, link)
+}, portfolioApp.outputContent = function (title, skills, content, port, link, ghLink)
 {
     $('body').prepend('<div class="u-portfolioEntry inactive text-center"></div>');
     setTimeout(function ()
@@ -49,7 +49,7 @@ portfolioApp.contentClick = function ()
     }, 100);
     setTimeout(function ()
     {
-        $('.u-portfolioEntry').html('<div class="row-fluid"><span class="u-closeButton u-animate">X</span><div class="col-sm-12 col-md-3" style="padding: 0;"><div class="menuBlock u-animate col-md-3"><h1 class="u-animate">'+title+'</h1><h3 class="u-animate">'+skills+'</h3><p class="u-marginTop u-animate">'+content+'</p><a href="'+link+'" class="u-animate buttonCustom">VIEW</a></div></div><div class="col-sm-12 col-md-9"><div class="u-thumbnail u-animate"><img class="img-responsive" src="/img/'+port+'/JPEG/1.jpg"></div><div class="u-thumbnail u-animate"><img class="img-responsive" src="/img/'+port+'/JPEG/1.jpg"></div><div class="u-thumbnail u-animate"><img class="img-responsive" src="/img/'+port+'/JPEG/1.jpg"></div></div></div>');
+        $('.u-portfolioEntry').html('<div class="row-fluid"><span class="u-closeButton u-animate">X</span><div class="col-sm-12 col-md-3" style="padding: 0;"><div class="menuBlock u-animate col-md-3"><h1 class="u-animate">'+title+'</h1><h3 class="u-animate">'+skills+'</h3><p class="u-marginTop u-animate">'+content+'</p><a href="'+link+'" class="u-animate buttonCustom">VIEW</a></div></div><div class="col-sm-12 col-md-9"><div class="u-thumbnail u-animate"><img class="img-responsive" src="/img/'+port+'/JPEG/1.jpg"></div><div class="u-thumbnail u-animate"><img class="img-responsive" src="/img/'+port+'/JPEG/2.jpg"></div><div class="u-thumbnail u-animate"><img class="img-responsive" src="/img/'+port+'/JPEG/3.jpg"></div></div></div>');
         $('.u-closeButton').unbind('click').bind('click', function ()
         {
             portfolioApp.removeContent();
